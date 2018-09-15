49c14411cfd14750af59b763a7342707ab10212b
hysumi <henrique.sumitomo@gmail.com>
2018-09-15 00:08:34 -0300

    adicionar evento de click no hexagono

--- src/components/hexagonGridComponent/hexagonComponent.js
+++ src/components/hexagonGridComponent/hexagonComponent.js
@@ -4,9 +4,37 @@ import React, { Component } from "react";
 import "../../Assets/css/skills.min.css";
 
 class Hexagonal extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            toggleClass: "hexagon",
+            toggleOn: true
+        };
+
+        this.toggleClick = this.toggleClick.bind(this);
+    }
+
+    toggleClick () {
+        this.setState({
+            toggleOn: !this.state.toggleOn
+        });
+
+        if (!this.state.toggleOn) {
+            this.setState({
+                toggleClass: "hexagon"
+            });
+        } else {
+            this.setState({
+                toggleClass: this.props.class
+            });
+        }
+    }
+
     render () {
+        console.log(this.state.toggleClass);
         return (
-            <div className={this.props.class}>
+            <div onClick={this.toggleClick} className={this.state.toggleClass}>
                 <svg role="img" viewBox={this.props.viewBox} xmlns="http://www.w3.org/2000/svg">
                     <circle cx={this.props.circle.cx} cy={this.props.circle.cy} r={this.props.circle.r}/>
                     {this.props.path.map((d, i) =>
