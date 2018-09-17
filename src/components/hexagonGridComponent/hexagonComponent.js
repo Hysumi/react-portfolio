226a27aa256b141bfb82bce4c3b2f716209e5544
Hysumi <henrique.sumitomo@gmail.com>
2018-09-16 23:21:09 -0300

    Ativar apenas um ícone de cada vez

--- src/components/hexagonGridComponent/hexagonComponent.js
+++ src/components/hexagonGridComponent/hexagonComponent.js
@@ -7,33 +7,22 @@ class Hexagonal extends Component {
     constructor (props) {
         super(props);
 
-        this.state = {
-            toggleClass: "hexagon",
-            toggleOn: true
-        };
-
         this.toggleClick = this.toggleClick.bind(this);
     }
 
     toggleClick () {
-        this.setState({
-            toggleOn: !this.state.toggleOn
-        });
-
-        if (!this.state.toggleOn) {
-            this.setState({
-                toggleClass: "hexagon"
-            });
-        } else {
-            this.setState({
-                toggleClass: "hex-selected " + this.props.class
-            });
-        }
+        this.props.changeActive(this.props.name);
     }
 
     render () {
+        let hexClass = "hexagon";
+
+        if (this.props.name === this.props.activeName) {
+            hexClass = "hex-selected " + this.props.class;
+        }
+
         return (
-            <div onClick={this.toggleClick} className={this.state.toggleClass}>
+            <div onClick={this.toggleClick} className={hexClass}>
                 <svg role="img" viewBox={this.props.viewBox} xmlns="http://www.w3.org/2000/svg">
                     <circle cx={this.props.circle.cx} cy={this.props.circle.cy} r={this.props.circle.r}/>
                     {this.props.path.map((d, i) =>
--- src/components/hexagonGridComponent/hexagonalGridComponent.js
+++ src/components/hexagonGridComponent/hexagonalGridComponent.js
@@ -10,11 +10,38 @@ import "../../Assets/css/skills.min.css";
 import Icons from "../../mocks/skill-icons-data";
 
 class HexagonalGrid extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            activeHex: ""
+        };
+
+        this.changeHexActive = this.changeHexActive.bind(this);
+    }
+
+    changeHexActive (newHex) {
+        if (this.state.activeHex === newHex) {
+            this.setState({
+                activeHex: ""
+            });
+        } else {
+            this.setState({
+                activeHex: newHex
+            });
+        }
+    }
+
+
     render () {
         return (
             <div className="hexagon-container">
                 {Icons.map((icon, i) =>
                     <Hexagonal key={i}
+                        changeActive={this.changeHexActive}
+                        activeName={this.state.activeHex}
+                        name={icon.name}
                         path={icon.path}
                         circle={icon.circle}
                         viewBox={icon.viewBox}
