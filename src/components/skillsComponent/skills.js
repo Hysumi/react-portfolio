8deeae3c5ddba9ba60f58719f67504a5ea65f33f
hysumi <henrique.sumitomo@gmail.com>
2018-09-30 15:43:23 -0300

    Alterar forma na página de Skills

--- src/components/skillsComponent/skills.js
+++ src/components/skillsComponent/skills.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 // components
-import HexagonalGrid from "../hexagonGridComponent/hexagonalGridComponent";
 
 // imports
 import "../../Assets/css/skills.min.css";
@@ -9,9 +8,9 @@ import "../../Assets/css/skills.min.css";
 class Skills extends Component {
     render () {
         return (
-            <div className="grid-container">
-                sdfsdfdsfsfs
-                <HexagonalGrid/>
+            <div className="skills-container">
+                <h1>Skills</h1>
+                <p>After thirteen years of searching, Anderson, a salesman from Los Angeles, found the envelope. It was empty.</p>
             </div>
         );
     }
