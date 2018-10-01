1c5181bd2393b4542c0c94467fb675f127ef0a5b
hysumi <henrique.sumitomo@gmail.com>
2018-09-30 21:05:04 -0300

    Cards tentativa 1

--- src/components/skillsComponent/skills.js
+++ src/components/skillsComponent/skills.js
@@ -10,7 +10,37 @@ class Skills extends Component {
         return (
             <div className="skills-container">
                 <h1>Skills</h1>
-                <p>After thirteen years of searching, Anderson, a salesman from Los Angeles, found the envelope. It was empty.</p>
+                <p>Classifiquei minhas habilidades em 3 Categorias: Básico, Intermediário e Avançado.</p>
+                <p>Em cada Categoria, classifico meu conhecimento técnico com base no que estudei e/ou desenvolvi em níveis de 1 à 10.</p>
+
+                <div className="skills-flex-container">
+                    <div className="cards techContainer teste">
+                        <div className="rulesContainer">
+                            <div className="titleCard">
+                                Básico
+                            </div>
+                            <div className="rulesContent">
+                                <ol className="levelsList">
+                                    <li className="levelItem">
+                                        Li sobre o assunto.
+                                    </li>
+                                    <li className="levelItem">
+                                        Estudei sobre o Assunto.
+                                    </li>
+                                    <li className="levelItem">
+                                        Implementei o básico para saber como usar.
+                                    </li>
+                                    <li className="levelItem">
+                                        Utilizei em um projeto, mas ainda não finalizei o projeto.
+                                    </li>
+                                    <li className="levelItem">
+                                        Utilizei em um projeto que está finalizado, mas não publicado.
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
