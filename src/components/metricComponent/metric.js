5071fec164cadf5289256b9b1d31699f5264181e
hysumi <henrique.sumitomo@gmail.com>
2018-10-22 00:25:07 -0300

    corrigindo nomeclatura do component de metrica

--- src/components/metricComponent/metric.js
+++ src/components/metricComponent/metric.js
@@ -9,33 +9,37 @@ class Metric extends Component {
             <article>
                 <section>
                     <h1 className="mainTitle">Metric</h1>
-
-                    <h5 className="title title--basic">Basic - Knowing the technology</h5>
-                    <h6 className="title title__level">Level 1:</h6>
+                </section>
+                <section>
+                    <h5 className="secTitle secTitle--basic">Basic - Knowing the technology</h5>
+                    <h6 className="secTitle secTitle__level">Level 1:</h6>
                     <p>I read about the content.</p>
-                    <h6 className="title title__level">Level 2:</h6>
+                    <h6 className="secTitle secTitle__level">Level 2:</h6>
                     <p>I studied the content.</p>
-                    <h6 className="title title__level">Level 3:</h6>
+                    <h6 className="secTitle secTitle__level">Level 3:</h6>
                     <p>I implemented the basic to know how to use the technology.</p>
-                    <h6 className="title title__level">Level 4:</h6>
+                    <h6 className="secTitle secTitle__level">Level 4:</h6>
                     <p>I used the technology in a project, but the project isn't concluded yet.</p>
-                    <h6 className="title title__level">Level 5:</h6>
+                    <h6 className="secTitle secTitle__level">Level 5:</h6>
                     <p>I used the technology and concluded the project.</p>
-
-                    <h5 className="title title--intermediate">Intermediate - Good practices</h5>
-                    <h6 className="title title__level">Level 6:</h6>
+                </section>
+                <section>
+                    <h5 className="secTitle secTitle--intermediate">Intermediate - Good practices</h5>
+                    <h6 className="secTitle secTitle__level">Level 6:</h6>
                     <p>I studied good practices.</p>
-                    <h6 className="title title__level">Level 7:</h6>
+                    <h6 className="secTitle secTitle__level">Level 7:</h6>
                     <p>I implemented the better practices to know how to use.</p>
-
-                    <h5 className="title title--advanced">Advanced - Mastering good practices.</h5>
-                    <h6 className="title title__level">Level 8:</h6>
+                </section>
+                <section>
+                    <h5 className="secTitle secTitle--advanced">Advanced - Mastering good practices.</h5>
+                    <h6 className="secTitle secTitle__level">Level 8:</h6>
                     <p>I used the good practices in a project, but the project isn't concluded yet.</p>
-                    <h6 className="title title__level">Level 9:</h6>
+                    <h6 className="secTitle secTitle__level">Level 9:</h6>
                     <p>I used the good practices and concluded the project.</p>
-
-                    <h5 className="title title--specialist">Expert - Specialist</h5>
-                    <h6 className="title title__level">Level 10:</h6>
+                </section>
+                <section>
+                    <h5 className="secTitle secTitle--specialist">Expert - Specialist</h5>
+                    <h6 className="secTitle secTitle__level">Level 10:</h6>
                     <p>Extra activities.</p>
                 </section>
             </article>
