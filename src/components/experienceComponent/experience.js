a6ef706dbda4fe3f248ccc7a8d23dbb23e0a04a7
hysumi <henrique.sumitomo@gmail.com>
2018-10-22 12:42:34 -0300

    Adicionar timeline

--- src/components/experienceComponent/experience.js
+++ src/components/experienceComponent/experience.js
@@ -9,85 +9,31 @@ class Experience extends Component {
     render () {
         return (
             <div className="elementContainer elementContainer--reverseColor">
-                <h1 className="mainTitle mainTitle--reverseColor">Experience</h1>
-                <section id="cd-timeline" className="cd-container">
-                    <div className="cd-timeline-block">
-                        <div className="cd-timeline-img cd-picture">
+                <h1 className="mainTitle mainTitle--reverseColor">Experience</h1>      
+                <section className="timeline__container">
+                    <div className="timeline__block">
+                        <div className="timeline__circle">
                         </div>
-
-                        <div className="cd-timeline-content">
-                            <h2>Penta Consulting</h2>
-                            <div className="timeline-content-info">
-                                <span className="timeline-content-info-title">
-                                    <i className="fa fa-certificate" aria-hidden="true"></i>
-                        Front End Developer
+                        <div className="timeline__content">
+                            <h2 className="mainTitle mainTitle--experience">Linx</h2>
+                            <div className=" timeline__content__info">
+                                <span className="timeline__content__info__title">
+                                    Full Stack Developer
                                 </span>
-                                <span className="timeline-content-info-date">
-                                    <i className="fa fa-calendar-o" aria-hidden="true"></i>
-                        June 2016 - Present
+                                <span className="timeline__content__info__date">
+                                    July 2017 - Present
                                 </span>
                             </div>
-                            <p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-                            <ul className="content-skills">
+                            <p>Management software company for the big retail sector serving the most diverse segments of this market.</p>
+                            <ul className="content__skills">
                                 <li>HTML5</li>
                                 <li>CSS3</li>
                                 <li>JavaScript</li>
-                                <li>jQuery</li>
-                                <li>Wordpress</li>
+                                <li>Git</li>
+                                <li>C# (.NET)</li>
+                                <li>Ionic 1</li>
+                                <li>SQL Server</li>
                             </ul>
-                        </div>
-                    </div>
-
-                    <div className="cd-timeline-block">
-                        <div className="cd-timeline-img cd-movie">
-                        </div>
-
-                        <div className="cd-timeline-content">
-                            <h2>Title of section 2</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p>
-                            <span className="cd-date">Jan 18</span>
-                        </div>
-                    </div>
-
-                    <div className="cd-timeline-block">
-                        <div className="cd-timeline-img cd-picture">
-                        </div>
-                        <div className="cd-timeline-content">
-                            <h2>Title of section 3</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
-                            <span className="cd-date">Jan 24</span>
-                        </div>
-                    </div>
-
-                    <div className="cd-timeline-block">
-                        <div className="cd-timeline-img cd-location">
-                        </div>
-
-                        <div className="cd-timeline-content">
-                            <h2>Title of section 4</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
-                            <span className="cd-date">Feb 14</span>
-                        </div>
-                    </div>
-
-                    <div className="cd-timeline-block">
-                        <div className="cd-timeline-img cd-location">
-                        </div>
-
-                        <div className="cd-timeline-content">
-                            <h2>Title of section 5</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum.</p>
-                            <span className="cd-date">Feb 18</span>
-                        </div>
-                    </div>
-
-                    <div className="cd-timeline-block">
-                        <div className="cd-timeline-img cd-movie">
-                        </div>
-                        <div className="cd-timeline-content">
-                            <h2>Final Section</h2>
-                            <p>This is the content of the last section</p>
-                            <span className="cd-date">Feb 26</span>
                         </div>
                     </div>
                 </section>
